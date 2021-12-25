110553dbb6083944e562d30f94c6dfcc15b42a62
Raunak Asnani <asnaniraunak@gmail.com>
2021-12-26 00:19:48 +0530

    Update cron & docker compose, review v1per

--- level_03/cron/cron.php
+++ level_03/cron/cron.php
@@ -1,9 +1,19 @@
 
 <?php
-    $con=mysqli_connect("db_flag","cyberlabs","CYB3RL4BS_!!FLAgs!","flag_storage") or die(mysql_error());
-    $query=mysqli_query($con,"SELECT flag FROM oswap WHERE chall=3");
-    $row = mysqli_fetch_array($query);
-    $flag=$row['flag'];
+    while ( 1 ) {
+    	sleep(5);
+        echo "Checking if db is up";
+	$con=mysqli_connect("db_flag","cyberlabs","CYB3RL4BS_!!FLAgs!","flag_storage");
+	if ( !$con ) {
+		continue;
+	}
+        $query=mysqli_query($con,"SELECT flag FROM oswap WHERE chall=3");
+        $row = mysqli_fetch_array($query);
+        $flag=$row['flag'];
+	if ( $flag && $flag !== '' ) {
+		break;
+	}
+    }
     $flagfile=fopen("/var/www/html/i_am_not_a_robot.txt","w");
     $txt=$flag;
     fwrite($flagfile,$txt);
